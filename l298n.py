732c93a171d6da559495cf486adc58573186f181
JBas <joshua.n.bas@gmail.com>
2019-05-20 20:26:36 -0400

    added self to the en variable

--- l298n.py
+++ l298n.py
@@ -6,15 +6,15 @@ class L298N:
         self.pinA = pinA
         self.pinB = pinB
 
-        en = 25
+        self.en = 25
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pinA, GPIO.OUT)
         GPIO.setup(self.pinB, GPIO.OUT)
-        GPIO.setup(en, GPIO.OUT)
+        GPIO.setup(self.en, GPIO.OUT)
         GPIO.output(self.pinA, GPIO.LOW)
         GPIO.output(self.pinB, GPIO.LOW)
 
-        self.p = GPIO.PWM(en, 1000)
+        self.p = GPIO.PWM(self.en, 1000)
         self.p.start(25)
 
         return
@@ -39,5 +39,5 @@ class L298N:
         return
 
     def clean(self):
-        GPIO.cleanup([self.pinA, self.pinB]);
+        GPIO.cleanup([self.pinA, self.pinB, self.en]);
         return
